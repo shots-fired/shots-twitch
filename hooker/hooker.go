60bcbd5c57d2e823158352405e880283fd86048e
Ryan <rwiesenberg603@gmail.com>
2018-10-27 16:49:56 -0400

    filling out AddStreamer

--- hooker/hooker.go
+++ hooker/hooker.go
@@ -1,9 +1,15 @@
 package hooker
 
-type streamerEncoding struct {
-	name string
-	id string
-}
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+const WebhookUrl = "https://api.twitch.tv/helix/webhooks/hub"
+const StreamStatusUrl = "https://api.twitch.tv/helix/streams"
+const UserInfoUrl = "https://api.twitch.tv/helix/users"
 
 type (
 	Hooker interface {
@@ -13,15 +19,19 @@ type (
 	}
 
 	hooker struct {
-		clientID             string
-		streamerEncodings []streamerEncoding
+		clientID              string
+		callbackURL           string
+		streamerEncodings     map[string]string
+		streamerSubscriptions map[string][]string
 	}
 )
 
-func NewHooker(clientID string) Hooker {
+func NewHooker(clientID string, callbackURL string) Hooker {
 	return hooker{
-		clientID:          clientID,
-		streamerEncodings: []streamerEncoding{},
+		clientID:              clientID,
+		callbackURL:           callbackURL,
+		streamerEncodings:     make(map[string]string),
+		streamerSubscriptions: make(map[string][]string),
 	}
 }
 
@@ -34,7 +44,25 @@ func (h hooker) AddStreamers(names []string) []error {
 }
 
 func (h hooker) AddStreamer(name string) error {
-	panic("implement me")
+	// Because webhooks require a user_id instead of a user_name we must find these from the api ourselves
+	getUserInfoResp, getUserInfoErr := http.Get(UserInfoUrl + "?login=" + name)
+	if getUserInfoErr != nil {
+		return getUserInfoErr
+	}
+	fmt.Println(getUserInfoResp)
+
+	// Now we need to construct the sub request to the webhook server
+	values := map[string]string{
+		"hub.topic":         StreamStatusUrl + "?user_id=" + h.streamerEncodings[name],
+		"hub.callback":      h.callbackURL,
+		"hub.lease_seconds": "600",
+	}
+	valuesJSON, _ := json.Marshal(values)
+	postWebhookResp, postWebhookErr:= http.Post(WebhookUrl, "application/json", bytes.NewBuffer(valuesJSON))
+	if postWebhookErr != nil {
+		return postWebhookErr
+	}
+	fmt.Println(name + ":> " + string(postWebhookResp.StatusCode))
 	return nil
 }
 
--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	var h hooker.Hooker = hooker.NewHooker("8a787t0q4tuqdgyd2fz6cgoug7q853")
+	var h hooker.Hooker = hooker.NewHooker("8a787t0q4tuqdgyd2fz6cgoug7q853", "http://shotsfired.xyz")
 
 	var streamerNames = []string{"Odega"}
 	h.AddStreamers(streamerNames)
